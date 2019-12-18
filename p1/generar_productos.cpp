16b80adaa9a9a7f3f36b31468f07eb91232bae2c
santibuey <38045718+santibuey@users.noreply.github.com>
2019-12-18 11:39:46 +0100

    datos y productos

---
+++ p1/generar_productos.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::string namefile = argv[1];
+    int n_datos = atoi(argv[2]);
+    std::vector<std::vector<bool>> M(n_datos, std::vector<bool>(n_datos));
+    std::uniform_real_distribution<float> dist_precio(10, 2000);
+    std::uniform_real_distribution<float> dist_unidades(100, 50000);
+
+    // Volcar datos en un fichero
+    std::ofstream f(namefile);
+    if (f.is_open()){   
+        int precio, unidades;
+        for (int i = 0; i < n_datos; i++){   
+            precio= dist_precio(mt);
+            unidades= dist_unidades(mt);
+            f << i+1 << "   " << "Producto_" << i+1 << "    " << precio 
+            << "    " << unidades << "\n" ;
+        }
+        f.close();
+    }
+    else{
+        std::cerr << "Fichero no creado" << std::endl;
+    }
+    return 0;
+}

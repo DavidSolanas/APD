86a55142424abde6e912aa53e8569401094b26a9
DavidSolanas <davidsolanas086@gmail.com>
2019-12-30 21:37:24 +0100

    cargar grafo en memoria

--- p1/pract1.cpp
+++ p1/pract1.cpp
@@ -1,47 +1,117 @@
 #include <iostream>
 #include <random>
-#include <cmath>
 #include <fstream>
-#include <vector>
+#include <set>
 
 using namespace std;
 
 // Estructura de datos para representar cada una de las aristas del grafo
-struct Arista{
-    // Cada arista se representa por los 2 vertices entre los que se encuentra:
-    int v1, v2; 
+class Arista
+{
+public:
+    // Cada arista se representa por los 2 vertices (v1 < v2 siempre)
+    int v1, v2;
+
+    //Constructor, siempre se asigna a v1 el menor de los valores de los parámetros
+    Arista(const int _v1, const int _v2)
+    {
+        if (_v1 < _v2)
+        {
+            v1 = _v1;
+            v2 = _v2;
+        }
+        else
+        {
+            v1 = _v2;
+            v2 = _v1;
+        }
+    }
+    const bool operator<(const Arista &a) const
+    {
+        int _a = v2 - v1;
+        int _b = a.v2 - a.v1;
+        return v1 < a.v1 || v2 < a.v2 || _a < _b;
+    }
+
+    const bool operator==(const Arista &a) const
+    {
+        return this->v1 == a.v1 && this->v2 == a.v2;
+    }
+
+    const bool operator!=(const Arista &a) const
+    {
+        return this->v1 != a.v1 || this->v2 != a.v2;
+    }
 };
 
 // Estructura de datos para representar el grafo sobre el que aplicamos Karger
-struct Grafo{
-    int V;  // Numero de vertices
-    int A;  // Numero de aristas
-    vector<Arista> aristas; // Grafo representado como un vector de aristas
+class Grafo
+{
+public:
+    int n_vertices;      // Numero de vertices
+    int n_aristas;       // Numero de aristas
+    set<Arista> aristas; // Grafo representado como un conjunto de aristas
+
+    //Constructor
+    Grafo() {}
+    Grafo(const int n_v, const int n_a, set<Arista> a) : n_vertices(n_v), n_aristas(n_a), aristas(a) {}
 };
 
+/**
+ *  Recupera el grafo del fichero especificado y lo almacena
+ *  en una estructura de datos de tipo Grafo.
+ */
+void cargar_grafo(ifstream &f, Grafo &grafo)
+{
+    int num_productos;
+    f >> num_productos;
+    grafo.n_vertices = num_productos;
+    bool G[num_productos][num_productos];
+    int num_aristas = 0;
+    for (int i = 0; i < num_productos; i++)
+    {
+        for (int j = 0; j < num_productos; j++)
+        {
+            bool b;
+            f >> b;
+            G[i][j] = b;
+            if (b)
+            {
+                num_aristas++;
+            }
+        }
+    }
+    num_aristas /= 2;
+    grafo.n_aristas = num_aristas;
+    for (int i = 0; i < num_productos; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (G[i][j])
+            {
+                grafo.aristas.insert(Arista(i, j));
+            }
+        }
+    }
+}
 
-int main(int argc, char const *argv[]){
-    string usage = "Usage: Path_tracer -n ppp";
-    if (argc != 2){
-        cerr << "Incorrect number of parameters" << std::endl;
+int main(int argc, char const *argv[])
+{
+    string usage = "Uso: pract1 <fichero_datos>";
+    Grafo grafo;
+    if (argc != 2)
+    {
+        cerr << "Número incorrecto de parámetros" << std::endl;
         cerr << usage << std::endl;
         exit(1);
     }
-    else{
-        string filename = argv[1];
-        ifstream f(filename);
-        if (f.is_open()){
-            int numProductos;
-            f >> numProductos;
-            cout << numProductos << endl;
-            for (int i=1; i++; i <= numProductos){
-                for (int j=1; j <= numProductos; j++){
 
-                }
-            }
-
-            f.close();
-        }
+    string filename = argv[1];
+    ifstream f(filename);
+    if (f.is_open())
+    {
+        cargar_grafo(f, grafo);
+        f.close();
     }
     return 0;
 }

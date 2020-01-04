f6dd5f05dc0c9fa1ecfcf45482c631306cf8b0e9
DavidSolanas <davidsolanas086@gmail.com>
2020-01-04 20:26:46 +0100

    karger-stein + conjunto vertices

--- p1/pract1.cpp
+++ p1/pract1.cpp
@@ -2,9 +2,34 @@
 #include <random>
 #include <fstream>
 #include <list>
+#include <vector>
 #include <map>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
+
+class Vertice
+{
+public:
+    int raiz;
+    list<int> vertices;
+
+    Vertice() { raiz = -1; }
+    Vertice(const int v) : raiz(v) { vertices.push_back(v); }
+    Vertice(const int v, list<int> _Vertices) : raiz(v), vertices(_Vertices) {}
+
+    void union_vertices(Vertice &n)
+    {
+        raiz = n.raiz < raiz ? n.raiz : raiz;
+        vertices.splice(vertices.end(), n.vertices);
+    }
+
+    const bool operator==(const Vertice &n) const
+    {
+        return this->raiz == n.raiz;
+    }
+};
 
 // Estructura de datos para representar cada una de las aristas del grafo
 class Arista
@@ -49,13 +74,21 @@ public:
 class Grafo
 {
 public:
-    int n_vertices;       // Numero de vertices
-    int n_aristas;        // Numero de aristas
-    list<Arista> aristas; // Grafo representado como un conjunto de aristas
+    int n_vertices; // Numero de vertices
+    int n_aristas;  // Numero de aristas
+    bool conexo;
+    list<Arista> aristas;     // Grafo representado como un conjunto de aristas
+    vector<Vertice> vertices; //Conjunto de vertices del grafo
 
     //Constructor
     Grafo() {}
-    Grafo(const int n_v, const int n_a, list<Arista> a) : n_vertices(n_v), n_aristas(n_a), aristas(a) {}
+    Grafo(const int n_v, const int n_a,
+          list<Arista> a, vector<Vertice> v) : n_vertices(n_v), n_aristas(n_a), aristas(a), vertices(v) {}
+
+    const bool operator<(const Grafo &G) const
+    {
+        return n_aristas < G.n_aristas;
+    }
 };
 
 //Obtiene directamente del fichero el grafo correspondiente
@@ -64,20 +97,29 @@ void cargar_grafo(ifstream &f, Grafo &grafo)
 {
     int num_productos;
     bool esArista;
+    bool conexo;
 
     f >> num_productos;
     grafo.n_vertices = num_productos;
+    grafo.vertices.resize(num_productos);
+    grafo.conexo = true;
 
     for (int i = 0; i < num_productos; i++)
     {
+        bool desconectado = true;
         for (int j = 0; j < num_productos; j++)
         {
             f >> esArista;
+            desconectado = desconectado & !esArista;
             if (j < i && esArista)
             {
                 grafo.aristas.push_back(Arista(i, j));
+                grafo.vertices[i] = Vertice(i);
+                grafo.vertices[j] = Vertice(j);
             }
         }
+        if (grafo.conexo && desconectado)
+            grafo.conexo = false;
     }
     grafo.n_aristas = grafo.aristas.size();
 }
@@ -93,9 +135,10 @@ void merge(Grafo &G, const int index)
 
     const int new_v = (*e).v1;
     const int v = (*e).v2;
-    //cout << "eliminando arista: <" << new_v << ", " << v << ">\n";
+
     // Actualiza el número de vértices del grafo G
     G.n_vertices--;
+    G.vertices[new_v].union_vertices(G.vertices[v]);
     // Borra la arista a del grafo G
     G.aristas.erase(e);
     auto it = G.aristas.begin();
@@ -119,34 +162,68 @@ void merge(Grafo &G, const int index)
     G.n_aristas = G.aristas.size();
 }
 
-int karger(Grafo &G)
+Grafo karger(const Grafo &G)
 {
     std::random_device rd;
     std::mt19937 mt(rd());
+    Grafo _G(G.n_vertices, G.n_aristas, G.aristas, G.vertices);
 
-    while (G.n_vertices > 2)
+    while (_G.n_vertices > 2)
     {
         // seleccionar arista aletoriamente
-        std::uniform_int_distribution<int> dist(0, G.n_aristas - 1);
+        std::uniform_int_distribution<int> dist(0, _G.n_aristas - 1);
         int i = dist(mt);
-        merge(G, i);
+        merge(_G, i);
     }
-    return G.n_aristas;
+    return _G;
 }
 
-int rep_karger(const Grafo &G, const int n)
+Grafo rep_karger(const Grafo &G, const int n)
 {
-    int min = G.n_aristas;
+    Grafo best_G;
+    best_G.n_aristas = G.n_aristas;
     for (int i = 0; i < n; i++)
     {
-        Grafo _G(G.n_vertices, G.n_aristas, G.aristas);
-        int k = karger(_G);
-        if (k < min)
+        Grafo _G = karger(G);
+        if (_G.n_aristas < best_G.n_aristas)
         {
-            min = k;
+            best_G = _G;
         }
     }
-    return min;
+    return best_G;
+}
+
+/**
+ * Aplica la contracción de aristas hasta que el garfo G tiene t vértices
+ */
+Grafo contract(const Grafo &G, const int t)
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    Grafo _G(G.n_vertices, G.n_aristas, G.aristas, G.vertices);
+    while (_G.n_vertices > t)
+    {
+        // seleccionar arista aletoriamente
+        std::uniform_int_distribution<int> dist(0, _G.n_aristas - 1);
+        int i = dist(mt);
+        merge(_G, i);
+    }
+    return _G;
+}
+
+Grafo karger_stein(const Grafo &G)
+{
+    if (G.n_vertices <= 6)
+    {
+        return karger(G);
+    }
+    else
+    {
+        int t = ceil(1 + G.n_vertices / M_SQRT2);
+        Grafo G1 = contract(G, t);
+        Grafo G2 = contract(G, t);
+        return min(karger_stein(G1), karger_stein(G2));
+    }
 }
 
 void cargar_productos(ifstream &f, map<int, string> &productos, int num_productos)
@@ -166,6 +243,38 @@ void cargar_productos(ifstream &f, map<int, string> &productos, int num_producto
     }
 }
 
+void calcular_desconectado(const Grafo &G, list<int> &v1, list<int> &v2)
+{
+    for (int i = 0; i < G.vertices.size(); i++)
+    {
+        if (G.vertices[i].raiz != -1)
+        {
+            //Vértice conectado
+            v1.push_back(i);
+        }
+        else
+        {
+            v2.push_back(i);
+        }
+    }
+}
+
+void mostrar_conjuntos(list<int> v1, list<int> v2)
+{
+    cout << "Conjunto vértices 1: {  ";
+    for (auto &&i : v1)
+    {
+        cout << i << "  ";
+    }
+    cout << "}\n";
+    cout << "Conjunto vértices 2: {  ";
+    for (auto &&i : v2)
+    {
+        cout << i << "  ";
+    }
+    cout << "}\n";
+}
+
 int main(int argc, char const *argv[])
 {
     string usage = "Uso: ./pract1 <fichero_datos> <fichero_productos>";
@@ -183,8 +292,36 @@ int main(int argc, char const *argv[])
     if (f.is_open())
     {
         cargar_grafo(f, grafo);
-        int n = karger(grafo);
-        cout << n << endl;
+        Grafo _g;
+
+        //Medir tiempo algoritmo
+        struct timespec start, finish;
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        if (grafo.conexo)
+        {
+            _g = karger_stein(grafo);
+            int _v1 = _g.aristas.front().v1;
+            int _v2 = _g.aristas.front().v2;
+            list<int> v1 = _g.vertices[_v1].vertices;
+            list<int> v2 = _g.vertices[_v2].vertices;
+            mostrar_conjuntos(v1, v2);
+        }
+        else
+        {
+            //Calcular solución manualmente
+            list<int> v1; //Conjunto de vértices 1 (conectados)
+            list<int> v2; //Conjunto de vértices 2 (desconectados)
+            calcular_desconectado(grafo, v1, v2);
+            mostrar_conjuntos(v1, v2);
+        }
+
+        // Obtener tiempo de ejecución
+        clock_gettime(CLOCK_REALTIME, &finish);
+        double elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+        cout << "Corte mínimo calculado: " << _g.n_aristas << endl;
+        std::cout << "Tiempo transcurrido del algoritmo: " << elapsed << " segundos." << std::endl;
         f.close();
     }
     else
